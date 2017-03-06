20fb2b156be22183eb864fb0cbdbf8380a182677
jfranc <jfranc@gmail.com>
2017-03-06 12:07:57 +0100

    Added to Gulp Source Maps for Javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 const gulp = require("gulp");
+const sourcemaps = require('gulp-sourcemaps');
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const plumber = require("gulp-plumber");
 
 gulp.task("min", function() {
-    gulp.src(["public/angularjs/miApp.js", "public/angularjs/ctrls/**/*.js"])
+    gulp.src(["angularjs/miApp.js", "angularjs/ctrls/**/*.js"])
         .pipe(plumber())
-        .pipe(concat("all.js"))
+        .pipe(sourcemaps.init())
+            .pipe(concat("all.js"))
+        .pipe(sourcemaps.write())
         .pipe(plumber.stop())
         .pipe(gulp.dest("public/dist/"));
 });
 
 gulp.task("watch", function() {
-    gulp.watch(["public/angularjs/miApp.js", "public/angularjs/ctrls/**/*.js"], ["min"]);
+    gulp.watch(["angularjs/miApp.js", "angularjs/ctrls/**/*.js"], ["min"]);
 });
