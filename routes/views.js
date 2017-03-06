c9a4dd96ec79f043e109ea93db5da8ffa619242e
jfranc <jfranc@gmail.com>
2017-03-06 10:20:35 +0100

    Unnecesary param title from routes - Delete them

--- routes/views.js
+++ routes/views.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/login', function(req, res, next) {
-    res.render('login', { title: 'Login' });
+    res.render('login');
 });
 
 router.get('/register', function(req, res, next) {
-    res.render('register', { title: 'Register' });
+    res.render('register');
 });
 
 router.get('/list', function(req, res, next) {
-    res.render('list' , { title: 'List' });
+    res.render('list');
 });
 
 module.exports = router;
